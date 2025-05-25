c25d6eadcdae2c52cee21c98b72c047cf8b5c852
Agung Kurniawan <agungk878@gmail.com>
2025-05-25 18:13:24 +0700

    add gitignore and refactor hapi js

--- index.js
+++ index.js
@@ -1,33 +1,29 @@
-const http = require('http');
+// load hapi
+const Hapi = require('@hapi/hapi');
+const { generateRandomNumber } = require('./utils');
 
-// define a simple request listener function
-function requestListener(request, response) {
+// Buat server
+const server = Hapi.server({ port: 3001, host: 'localhost' });
 
-    const { method, url } = request;
+// Buat rute
+const routes = [
+  {
+    method: 'GET',
+    path: '/',
+    handler: () => ({ message: 'Hello, world!' })
+  },
+  {
+    method: 'GET',
+    path: '/lucky-number',
+    handler: () => ({ luckyNumber: generateRandomNumber() })
+  }
+];
 
-    response.setHeader('Content-Type', 'application/json');
-    response.statusCode = 200;
-    
+server.route(routes);
 
-    // routing
+const start = async () => {
+  await server.start();
+  console.log('Server running on', server.info.uri);
+};
 
-    if(url === '/' && method === 'GET') {
-        response.end(JSON.stringify({ message: 'Welcome to the API' }));
-    }
-
-    if(url == '/lucky-number' && method === 'GET') {
-        const luckyNumber = Math.floor(Math.random() * 100) + 1;
-        response.end(JSON.stringify({ luckyNumber }));
-    }
-
-
-
-}
-
-// buat http server
-const server = http.createServer(requestListener);
-
-// running the http server
-server.listen(3001, 'localhost', () => {
-    console.log('Server is running on http://localhost:3001');
-})
+start();
